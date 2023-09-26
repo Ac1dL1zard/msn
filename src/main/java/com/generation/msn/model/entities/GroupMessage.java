7e2c639793896415f5b47558b88b35d2aaffbc21
titov <titov@LAPTOP-VDFEK20G.homenet.telecomitalia.it>
2023-09-26 18:48:33 +0200

    Aggiustato group message

--- src/main/java/com/generation/msn/model/entities/GroupMessage.java
+++ src/main/java/com/generation/msn/model/entities/GroupMessage.java
@@ -1,6 +1,9 @@
 package com.generation.msn.model.entities;
 
 import java.time.LocalDate;
+import java.util.List;
+
+import com.generation.msn.library.BaseEntity;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.JoinColumn;
@@ -16,7 +19,7 @@ import lombok.Setter;
 @AllArgsConstructor
 @NoArgsConstructor
 @Entity
-public class GroupMessage 
+public class GroupMessage extends BaseEntity
 {
 	private String content,sender_nickname;
 	private LocalDate sending_date_time;	
@@ -24,4 +27,10 @@ public class GroupMessage
 	@ManyToOne
 	@JoinColumn(name="group_chat_id")
 	GroupChat groupchat = new GroupChat();
+
+	@Override
+	public List<String> getErrors() {
+		// TODO Auto-generated method stub
+		return null;
+	}
 }
